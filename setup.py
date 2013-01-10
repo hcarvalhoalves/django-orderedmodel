943962cc1a618083c0dc4e273a081cc2ef9e7309
Henrique Carvalho Alves <hcarvalhoalves@gmail.com>
2013-01-10 17:20:58 -0200

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django-orderedmodel',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False)
